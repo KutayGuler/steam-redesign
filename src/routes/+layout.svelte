b143dbead92fce19d644cac78da3e2719d5720f2
KutayGuler <s7019@horyzont.eu>
2023-10-09 21:33:09 +0200

    latest changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,17 @@
   import '../app.css';
   import { fade, fly } from 'svelte/transition';
   function openHamburger() {
-    hamburgerOpen = true;
+    open = true;
   }
-  let hamburgerOpen = false;
+  let open = false;
   const links = [];
 </script>
 
-<main class="w-full h-full {hamburgerOpen ? 'overflow-hidden' : ''}">
+<main class="w-full h-full {open ? 'overflow-hidden' : ''}">
   <nav
-    class="z-20 w-full flex flex-row items-center justify-between p-4 pl-0 bg-gradient-to-b from-black via-black to-transparent fixed"
+    class="{open
+      ? 'z-10'
+      : 'z-20'} w-full flex flex-row items-center justify-between p-4 pl-0 bg-gradient-to-b from-black via-black to-transparent fixed"
   >
     <img
       class="w-40"
@@ -25,18 +27,18 @@
       />
     </button>
   </nav>
-  {#if hamburgerOpen}
+  {#if open}
     <div class="fixed z-20 w-2/3 h-full bg-slate-950 right-0" transition:fly={{ x: 100 }}>
       {#each links as link}
         <a href="/">{link}</a>
       {/each}
     </div>
     <div
-      class="fixed z-10 bg-black opacity-80 w-full h-full"
+      class="fixed z-[11] bg-black opacity-80 w-full h-full"
       transition:fade
       on:click|self={() => {
         console.log('clicked');
-        hamburgerOpen = false;
+        open = false;
       }}
     />
   {/if}
