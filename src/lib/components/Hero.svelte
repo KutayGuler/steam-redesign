dfab5eccfd65364803335ba3d013524700c6457e
KutayGuler <s7019@horyzont.eu>
2023-10-25 14:03:09 +0200

    added profile

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,9 +1,20 @@
-<script lang="ts">
+<script>
   // TODO: slideable featured products
+  import Carousel from 'svelte-carousel';
 </script>
 
-<img
+<Carousel>
+  {#each { length: 3 } as item}
+    <img
+      class="w-full h-full pb-4"
+      src="https://images.unsplash.com/photo-1635205411883-ae35d1169f60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTUxfHx2aWRlbyUyMGdhbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+      alt=""
+    />
+  {/each}
+</Carousel>
+
+<!-- <img
   class="w-full h-full pb-4"
   src="https://images.unsplash.com/photo-1635205411883-ae35d1169f60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTUxfHx2aWRlbyUyMGdhbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
   alt=""
-/>
+/> -->
---
+++ src/routes/profile/[id]/+page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+  const comments = [
+    {
+      user: 'user',
+      comment: 'comment'
+    },
+    {
+      user: 'user',
+      comment: 'comment'
+    },
+    {
+      user: 'user',
+      comment: 'comment'
+    },
+    {
+      user: 'user',
+      comment: 'comment'
+    },
+    {
+      user: 'user',
+      comment: 'comment'
+    }
+  ];
+</script>
+
+{$page.params.id}
+
+<main class="flex flex-col p-4 text-white">
+  <div class="flex flex-row gap-2">
+    <img src="https://placehold.co/120x120" alt="" />
+    <div class="flex flex-col">
+      <h1 class="text-4xl">user name</h1>
+      <p>Country</p>
+      <p>Description</p>
+    </div>
+  </div>
+  <div>
+    <h1>Comments</h1>
+    <div class="flex flex-col gap-2">
+      {#each comments as comment}
+        <div class="flex flex-row gap-2">
+          <img src="https://placehold.co/60x60" alt="" />
+          <div class="flex flex-col gap-1">
+            <h3>{comment.user}</h3>
+            <p>{comment.comment}</p>
+          </div>
+        </div>
+      {/each}
+    </div>
+  </div>
+</main>
