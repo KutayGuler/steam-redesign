ec5f77828076bfaf6583ac1be9806f0c33adb5bf
KutayGuler <s7019@horyzont.eu>
2023-11-08 18:29:56 +0300

    carousel

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,17 +1,22 @@
 <script>
-  // TODO: slideable featured products
   import Carousel from 'svelte-carousel';
 </script>
 
-<Carousel>
+<Carousel autoplay autoplayDuration={5000} arrows={false}>
   {#each { length: 3 } as item}
-    <img
-      class="w-full h-full pb-4"
-      src="https://images.unsplash.com/photo-1635205411883-ae35d1169f60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTUxfHx2aWRlbyUyMGdhbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-      alt=""
-    />
+    <div class="relative">
+      <h3 class="absolute bottom-16 left-8 text-white text-2xl font-bold">
+        Stray Panda
+      </h3>
+      <img
+        class="w-full h-full pb-4"
+        src="https://images.unsplash.com/photo-1635205411883-ae35d1169f60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTUxfHx2aWRlbyUyMGdhbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+        alt=""
+      />
+    </div>
   {/each}
 </Carousel>
+<div class="pb-8" />
 
 <!-- <img
   class="w-full h-full pb-4"
